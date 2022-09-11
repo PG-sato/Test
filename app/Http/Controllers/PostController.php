73eafc20fcf4463d7929ea8816e3d2274ea3736c
PG-sato <kokoko012146@gmail.com>
2022-09-11 16:42:25 +0900

    エラー処理機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+// use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -19,7 +20,7 @@ class PostController extends Controller
 	{
 		return view('create');
 	}
-	public function store(Post $post, Request $request)
+	public function store(Post $post, PostRequest $request)
 	{
 		$input = $request['post'];
 		$post->fill($input)->save();
